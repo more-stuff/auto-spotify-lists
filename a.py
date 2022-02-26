25cc9e55e9f86ac1d65fb3ffce59a949924c28d5
z3hcnas <65340565+z3hcnas@users.noreply.github.com>
2022-02-26 13:45:45 +0100

    change the default version for the execution

--- a.py
+++
@@ -1,7 +0,0 @@
-def div_num(num):
-    for i in range(num):
-        if i != 0:
-            if num % i == 0:
-                print(i)
-
-div_num(256)
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ client_secret = '502b106fd2f64280a642c7bf2644c6bd'
 redirect_uri = 'https://github.com/z3hcnas'
 spotify_name = 'lol'
 spotify_id_new = 'zj4jfhtfl7h6vqmidwzeixdub'
-version = 'cutre'
+version = ''
 
 if version == 'cutre':
     token = auth.cutre_version()
